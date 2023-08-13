cf7e370f44bd6765998938a5fb49f676174c8b0e
Andrew Oliveira <andrewhenriquef@gmail.com>
2023-08-13 20:34:01 -0300

    (=) Give meaningfull names to variables

--- log_reader.rb
+++ log_reader.rb
@@ -10,49 +10,69 @@ players = []
 current_match_kills = 0
 log_kills = []
 
-def render_current_game_data(game_id, total_kills, players, log_kills)
+def render_match_data(game_id, total_kills, players, log_kills)
   {
     "game_#{game_id}" => {
       "total_kills" => total_kills,
       "players" => players.uniq,
-      "kills" => generate_kills(players, log_kills),
+      "kills" => generate_kills_per_player(players, log_kills),
       "kills_by_means" => generate_kills_by_means(log_kills)
     }
   }
 end
 
-def generate_kills(players, log_kills)
+def generate_kills_per_player(players, log_kills)
   return {} if log_kills.empty?
 
-  world_deaths = log_kills.select { |killer, dead, mod| killer == '<world>'}.group_by { |item| item[1] }.map do |player, deaths_by_world|
+  players_kills = count_of_players_kills(log_kills)
+  worlds_kills = count_of_players_deaths_by_world(log_kills)
+
+  kills_per_player = players.map do |player|
+    player_kills = players_kills.find { |kills| kills[player] }
+    player_deaths = worlds_kills.find { |deaths| deaths[player] }
+
+    kills = entity_kills(player_kills, player)
+
+    deaths_by_world = entity_kills(player_deaths, player)
+
+    { player => kills - deaths_by_world }
+  end
+
+  kills_per_player.inject(:merge)
+end
+
+def count_of_players_deaths_by_world(log_kills)
+  world_kills = log_kills.select { |killer, dead, mod| killer == '<world>'}
+  world_kills_grouped_by_killed = world_kills.group_by { |item| item[1] }
+  world_kills_grouped_by_killed.map do |player, deaths_by_world|
     { player => deaths_by_world.count }
   end
+end
 
-  grouped_kills_per_players = log_kills.reject { |killer, dead, mod| killer == '<world>'}.group_by(&:first).map do |player, kills|
+def count_of_players_kills(log_kills)
+  player_kills = log_kills.reject { |killer, dead, mod| killer == '<world>'}
+  player_kills_grouped_by_killer = player_kills.group_by(&:first)
+  player_kills_grouped_by_killer.map do |player, kills|
     { player => kills.count }
   end
+end
 
-  kills_per_player = {}
+def entity_kills(kills_data, player)
+  return 0 if kills_data.nil?
 
-  players.each do |player|
-    player_kills = grouped_kills_per_players.find { |kills| kills[player] }
-    player_deaths = world_deaths.find { |deaths| deaths[player] }
-
-    kills = player_kills ? player_kills.dig(player) : 0
-    deaths_by_world = player_deaths ? player_deaths.dig(player) : 0
-
-    kills_per_player[player] = kills - deaths_by_world
-  end
-
-  kills_per_player
+  kills_data.dig(player)
 end
 
 def generate_kills_by_means(log_kills)
-  log_kills.group_by do |log_kill|
-    log_kill[2]
-  end.map do |mod, kills|
+  return {} if log_kills.empty?
+
+  kills_grouped_by_mod = log_kills.group_by { |log_kill| log_kill[2] }
+
+  kills_by_mods = kills_grouped_by_mod.map do |mod, kills|
     { mod => kills.count }
-  end.inject(:merge)
+  end
+
+  kills_by_mods.inject(:merge)
 end
 
 File.foreach(FILE_PATH) do |raw_log_line|
